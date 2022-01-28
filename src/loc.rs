3dfdec00ff09af7a4f0b05a1c691d4213ad6f267
Jonas Grawe <Shemnei@users.noreply.github.com>
2022-01-28 20:52:05 +0100

    Made Loc fields public and added Display impl

--- src/loc.rs
+++ src/loc.rs
@@ -1,11 +1,19 @@
+use std::fmt;
+
 use crate::pos::Pos;
 
 /// A location inside a [`Source`](`crate::src::Source`).
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Loc {
 	/// Zero indexed line.
-	line: usize,
+	pub line: usize,
 
 	/// Zero indexed column.
-	column: Pos,
+	pub column: Pos,
+}
+
+impl fmt::Display for Loc {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "{}:{}", self.line + 1, self.column.as_u32() + 1)
+	}
 }
